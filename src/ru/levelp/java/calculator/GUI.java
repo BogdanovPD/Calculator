c16bc8a1d6b473d027814d448e6bc58066cc6083
BogdanovPD <bogdanovpd@gmail.com>
2015-07-23 15:55:49 +0300

    Commit

--- src/ru/levelp/java/calculator/GUI.java
+++ src/ru/levelp/java/calculator/GUI.java
@@ -13,7 +13,10 @@ public class GUI {
     private ArrayList<JButton> buttons = new ArrayList<JButton>();
     private GridLayout gridLayoutNumbers = new GridLayout(4, 3);
     private GridLayout gridLayoutOperators = new GridLayout(2, 2);
-    private GridLayout gridLayoutMemory = new GridLayout(4,1);
+    private GridLayout gridLayoutLeftSide = new GridLayout(1, 3);
+    private GridLayout gridLayoutMemory = new GridLayout(4, 1);
+    private GridLayout gridLayoutTrig = new GridLayout(4, 1);
+    private GridLayout gridLayoutOther = new GridLayout(4, 1);
     private JPanel panel = new JPanel();
     private JPanel panelOperators = new JPanel();
     private JPanel panelEquals = new JPanel();
@@ -25,19 +28,29 @@ public class GUI {
     private JPanel panelLabel = new JPanel();
     private JPanel panelRight = new JPanel();
     private JPanel panelMemory = new JPanel();
+    private JPanel panelTrig = new JPanel();
+    private JPanel panelOther = new JPanel();
+    private JPanel panelLeftSide = new JPanel();
     private JLabel textField = new JLabel("0");
     private double current;
+    private double lastCurrent;
     private double memory;
     private String operation = "";
-    private  boolean clear = false;
+    private String lastOperation = "";
+    private boolean clear = false;
     private NumButtonListener NBL = new NumButtonListener(this);
     private OperationListener aL = new OperationListener(this);
+    private JMenuBar bar = new JMenuBar();
+    private JMenu menu = new JMenu("View");
+    private JMenuItem menuItem1 = new JMenuItem("Standart");
+    private JMenuItem menuItem2 = new JMenuItem("Extended");
+    private int counter=0;
 
-    public void build(){
+    public void build() {
         JFrame frame = new JFrame("Calculator");
         //frameOperators.add(panelOperators);
         //frameOperators.add(panelEquals);
-        frame.setBounds(100, 100, 350, 150);
+        frame.setBounds(100, 100, 300, 174);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         panel.setLayout(gridLayoutNumbers);
         panelOperators.setLayout(gridLayoutOperators);
@@ -50,32 +63,37 @@ public class GUI {
         panelLabel.setLayout(new BorderLayout());
         panelRight.setLayout(new BorderLayout());
         panelMemory.setLayout(gridLayoutMemory);
+        panelTrig.setLayout(gridLayoutTrig);
+        panelOther.setLayout(gridLayoutOther);
+        panelLeftSide.setLayout(gridLayoutLeftSide);
         Font font = getTextField().getFont();
         getTextField().setFont(new Font(font.getFontName(), font.getStyle(), font.getSize() + 10));
         textField.setHorizontalAlignment(JLabel.RIGHT);
         textField.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createBevelBorder(BevelBorder.LOWERED),
                 BorderFactory.createEmptyBorder(1, 1, 1, 1)));
+        panelLeftSide.setVisible(false);
+        //textField.setMinimumSize(new Dimension(100, 10));
 
 
-        int j=0;
+        int j = 0;
         //789 456 123 = 012 345 678(ind)
         for (int i = 7; i < 10; i++) {
-            buttons.add(new JButton((""+i)));
+            buttons.add(new JButton(("" + i)));
             buttons.get(j).addActionListener(NBL);
             panel.add(buttons.get(j));
             j++;
         }
 
         for (int i = 4; i < 7; i++) {
-            buttons.add(new JButton((""+i)));
+            buttons.add(new JButton(("" + i)));
             buttons.get(j).addActionListener(NBL);
             panel.add(buttons.get(j));
             j++;
         }
 
         for (int i = 1; i < 4; i++) {
-            buttons.add(new JButton((""+i)));
+            buttons.add(new JButton(("" + i)));
             buttons.get(j).addActionListener(NBL);
             panel.add(buttons.get(j));
             j++;
@@ -89,7 +107,7 @@ public class GUI {
         buttons.get(10).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textField.setText("-" + textField.getText());
+                setTextField("-" + textField.getText());
             }
         });
         panel.add(buttons.get(10));
@@ -99,7 +117,7 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (!textField.getText().contains(".")) {
-                    textField.setText(textField.getText() + ".");
+                    setTextField(textField.getText() + ".");
                 }
             }
         });
@@ -160,8 +178,8 @@ public class GUI {
         buttons.get(20).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String mr = (""+memory);
-                textField.setText(mr.endsWith(".0") ? mr = mr.substring(0, mr.length() - 2) : mr);
+                String mr = ("" + memory);
+                setTextField(cutInsignificantZeros(mr));
 
             }
         });
@@ -184,10 +202,95 @@ public class GUI {
         panelMemory.add(buttons.get(21));
         panelMemory.add(buttons.get(22));
 
+        buttons.add(new JButton(("Sin")));
+        buttons.get(23).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) Math.sin(Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+        buttons.add(new JButton(("Cos")));
+        buttons.get(24).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) Math.cos(Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+        buttons.add(new JButton(("Tg")));
+        buttons.get(25).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) Math.tan(Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+        buttons.add(new JButton(("Ctg")));
+        buttons.get(26).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) (1.0 / Math.tan(Double.parseDouble(textField.getText())))).toString()));
+            }
+        });
+        buttons.add(new JButton(("Log")));
+        buttons.get(27).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) Math.log(Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+        buttons.add(new JButton(("%")));
+        buttons.get(28).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) (current / 100.0 * Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+        buttons.add(new JButton(("x^")));
+        buttons.get(29).addActionListener(aL);
+        buttons.add(new JButton(("sqrt")));
+        buttons.get(30).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setTextField(cutInsignificantZeros(((Double) Math.sqrt(Double.parseDouble(textField.getText()))).toString()));
+            }
+        });
+
+        panelTrig.add(buttons.get(23));
+        panelTrig.add(buttons.get(24));
+        panelTrig.add(buttons.get(25));
+        panelTrig.add(buttons.get(26));
+
+        panelOther.add(buttons.get(27));
+        panelOther.add(buttons.get(28));
+        panelOther.add(buttons.get(29));
+        panelOther.add(buttons.get(30));
+
+        panelLeftSide.add(panelOther);
+        panelLeftSide.add(panelTrig);
+        panelLeftSide.add(panelMemory);
+
 
         panelLabel.add(getTextField());
 
+        menuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelLeftSide.setVisible(false);
+                frame.setBounds(100, 100, 300, 174);
+            }
+        });
 
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelLeftSide.setVisible(true);
+                frame.setBounds(100, 100, 500, 174);
+            }
+        });
+
+        menu.add(menuItem1);
+        menu.add(menuItem2);
+        bar.add(menu);
+        frame.setJMenuBar(bar);
 
         frame.add(BorderLayout.CENTER, panel);
 
@@ -204,7 +307,7 @@ public class GUI {
         panelRight.add(BorderLayout.CENTER, panelBackspaceC_Operators_Main);
 
         frame.add(BorderLayout.EAST, panelRight);
-        frame.add(BorderLayout.WEST, panelMemory);
+        frame.add(BorderLayout.WEST, panelLeftSide);
 
         frame.setResizable(false);
         frame.setVisible(true);
@@ -232,7 +335,10 @@ public class GUI {
     }
 
     public void setTextField(String text) {
-        this.textField.setText(text);
+        if (text.length() <= 11)
+            this.textField.setText(text);
+        else
+            this.textField.setText(text.substring(0, 11));
     }
 
     public boolean isClear() {
@@ -241,5 +347,34 @@ public class GUI {
 
     public void setClear(boolean clear) {
         this.clear = clear;
+    }
+
+    public String cutInsignificantZeros(String text) {
+        return (text.endsWith(".0") ? text.substring(0, text.length() - 2) : text);
+    }
+
+
+    public String getLastOperation() {
+        return lastOperation;
+    }
+
+    public void setLastOperation(String lastOperation) {
+        this.lastOperation = lastOperation;
+    }
+
+    public double getLastCurrent() {
+        return lastCurrent;
+    }
+
+    public void setLastCurrent(double lastCurrent) {
+        this.lastCurrent = lastCurrent;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
--- src/ru/levelp/java/calculator/OperationListener.java
+++ src/ru/levelp/java/calculator/OperationListener.java
@@ -14,34 +14,33 @@ public class OperationListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-//        if (((JButton) e.getSource()).getText().equals("+")) {
-//            if (gui.getOperation().equals("+")) {
-//                double labelText = Double.parseDouble(gui.getTextField().getText());
-//                String newText = "" + (labelText + gui.getCurrent());
-//                gui.setTextField("" + (labelText + gui.getCurrent()));
-//                gui.setTextField(newText.endsWith(".0") ? newText = newText.substring(0, newText.length() - 2) : newText);
-//                gui.setOperation("");
-//                gui.setClear(false);
-//            }
-//            gui.setCurrent(Double.parseDouble(gui.getTextField().getText()));
-//            gui.setClear(false);
-//            gui.setOperation("+");
-//        }
-
         useOperation(((JButton) e.getSource()).getText(), e);
-
     }
 
 
     public void useOperation(String op, ActionEvent e) {
 
-            if (gui.getOperation().equals("=")){
-                gui.setOperation("");
+//            if ((!gui.getLastOperation().equals("=")) && (!gui.getLastOperation().equals("")))
+//                gui.setOperation(op);
+
+            if (gui.getOperation().equals("=") ){
+                if (gui.getCounter()>0 && op.equals("="))
+                    gui.setOperation(gui.getLastOperation());
+                else
+                    gui.setOperation("");
+            }
+            else {
+                gui.setCounter(0);
             }
 
             if (!gui.getOperation().equals("")) {
                 String newText = "";
-                double labelText = Double.parseDouble(gui.getTextField().getText());
+                double labelText;
+                if ((!op.equals("=")) || (gui.getCounter()==0))
+                    labelText = Double.parseDouble(gui.getTextField().getText());
+                else{
+                    labelText = gui.getLastCurrent();
+                    gui.setCounter(gui.getCounter()+1);}
                 if (gui.getOperation().equals("+"))
                     newText = "" + (labelText + gui.getCurrent());
                 if (gui.getOperation().equals("*"))
@@ -50,21 +49,36 @@ public class OperationListener implements ActionListener {
                     newText = "" + (gui.getCurrent() - labelText);
                 if (gui.getOperation().equals("/"))
                     newText = "" + (gui.getCurrent() / labelText);
+                if (gui.getOperation().equals("x^"))
+                    newText = "" + (Math.pow(gui.getCurrent(),labelText));
+                if (gui.getCounter()==0)
+                    gui.setLastCurrent(labelText);
+                gui.setLastOperation(gui.getOperation());
                 gui.setTextField(newText);
-                gui.setTextField(newText.endsWith(".0") ? newText = newText.substring(0, newText.length() - 2) : newText);
+                gui.setTextField(gui.cutInsignificantZeros(newText));
                 gui.setOperation("=");
                 gui.setClear(false);
             }
             gui.setCurrent(Double.parseDouble(gui.getTextField().getText()));
 
-            if (op.equals("+"))
-                gui.setOperation("+");
-            if (op.equals("*"))
-                gui.setOperation("*");
-            if (op.equals("-"))
-                gui.setOperation("-");
-            if (op.equals("/"))
-                gui.setOperation("/");
+            if (op.equals("+")){
+                gui.setLastOperation(gui.getOperation());
+                gui.setOperation("+");}
+            if (op.equals("*")){
+                gui.setLastOperation(gui.getOperation());
+                gui.setOperation("*");}
+            if (op.equals("-")){
+                gui.setLastOperation(gui.getOperation());
+                gui.setOperation("-");}
+            if (op.equals("/")){
+                gui.setLastOperation(gui.getOperation());
+                gui.setOperation("/");}
+            if (op.equals("x^")){
+                gui.setLastOperation(gui.getOperation());
+                gui.setOperation("x^");}
+            if (op.equals("="))
+                gui.setCounter(gui.getCounter()+1);
             gui.setClear(false);
+
     }
 }
